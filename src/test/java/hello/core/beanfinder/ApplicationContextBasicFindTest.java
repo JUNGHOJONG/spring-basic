ba3c699e5df3408878e52d65bee02d80f082eea0
JUNGHOJONG <hojong1351@naver.com>
2021-07-18 19:37:47 +0900

    스프링 빈 조회 - 기본

---
+++ src/test/java/hello/core/beanfinder/ApplicationContextBasicFindTest.java
@@ -0,0 +1,46 @@
+package hello.core.beanfinder;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+// Junit5 에서는 public 쓰지 않아도 자동설정 된다
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회하기")
+    // Junit5 에서는 public 쓰지 않아도 자동설정 된다
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름없이 타입만으로 조회하기")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회하기")
+    void findBeanByType2() {
+        MemberServiceImpl memberServiceImpl = ac.getBean(MemberServiceImpl.class);
+        assertThat(memberServiceImpl).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("", MemberService.class));
+    }
+}
