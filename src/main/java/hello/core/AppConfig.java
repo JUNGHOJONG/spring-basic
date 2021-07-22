b82447f8ee6e41eb21430acdf5f44409b122eeff
JUNGHOJONG <hojong1351@naver.com>
2021-07-22 19:20:57 +0900

    @Configuration과 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,23 +14,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // 예상 : call1, call2, call2, call3, call2, call4
+    // 결과 : call1, call2, call3, call4
+
     @Bean
     public MemberService memberService() {
+        System.out.println("call1 AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call2 AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call3 AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     @Bean
     public DiscountPolicy discountPolicy() {
+        System.out.println("call4 AppConfig.discountPolicy");
         return new RateDiscountPolicy();
     }
 }
