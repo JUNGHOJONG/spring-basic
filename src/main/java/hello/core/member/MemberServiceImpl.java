865473113687b497c5cfb4f18138f4fdfcd71743
JUNGHOJONG <hojong1351@naver.com>
2021-07-29 21:30:35 +0900

    컴포넌트 스캔과 의존관계 자동 주입 시작하기까지 완료
    - AutoAppConfig 의 위치는 hello.core 디렉토리 아래에 속해야한다(만약 policy 등의 디렉토리 아래에 두면 NoSuchBeanDefinitionException 에러가 난다)

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     // 왜 final를 쓰고 static은 안쓰는가?? -> 메모리 관리(객체의 라이플 사이클)
     private final MemberRepository memberRepository;
 
+    @Autowired // ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
